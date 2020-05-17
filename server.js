c807074e9092947b750d2d6ccf28747adf71e32b
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-17 10:34:58 +0530

    use cors in server

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+var corsOption = {
+  origin: ["http://localhost:4200", "http://localhost:4000"],
+};
+
+app.use(cors(corsOption));
 
 app.listen(8000, () => {
   console.log("Server is started and listening");
